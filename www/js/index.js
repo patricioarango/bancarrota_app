f77218dc398d754ee723855667763de7194885e0
patricioarango <patricioarango@gmail.com>
2017-06-30 16:07:39 -0300

    magic

--- www/js/index.js
+++ www/js/index.js
@@ -179,6 +179,7 @@ function guardar_transaccion(id_transaccion,importe,id_subcategoria,observacion)
     localStorage.setItem("bancarrota_id_subcategoria_" + id_transaccion,id_subcategoria); 
     localStorage.setItem("bancarrota_importe_" + id_transaccion,importe); 
     localStorage.setItem("bancarrota_observacion_" + id_transaccion,observacion); 
+    calcular_cantidades();
 }
 
 function limpiar_transaccion(id_subcategoria,posicion){
@@ -204,4 +205,28 @@ function sincronizar_transacciones(){
             localStorage.removeItem("id_transaccion");
         }
     }
+}
+
+function calcular_cantidades(){
+    calcular_enviadas();
+     calcular_para_sincronizar();
+}
+function calcular_enviadas(){
+   db.ref('/bancarrota/transacciones/no_procesadas').orderByChild('google_id').equalTo(user.uid).on('value', function(snapshot) {
+      var transacciones = snapshot.val();
+        if (transacciones == null){
+            $("#enviadas").text("0");        
+        } else {
+            $("#enviadas").text(transacciones.length.toString());
+        }
+    });  
+} 
+
+function calcular_para_sincronizar(){
+    var transacciones2 = localStorage.getItem("id_transaccion");   
+        if (transacciones2 == null){
+            $("#para_sincronizar").text("0");        
+        } else {
+            $("#para_sincronizar").text(transacciones2.length.toString());
+        }
 }    
