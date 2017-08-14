64afc3aab0b710762a11b2159f9aae550ac2eab9
patricioarango <patricioarango@gmail.com>
2017-08-14 16:19:51 -0300

    cambia modo insert html

--- www/js/index.js
+++ www/js/index.js
@@ -335,4 +335,10 @@ $(function(){
     $("#user_email").html(email);
    }
     
+    $('.mostrar_nav').sideNav({
+          menuWidth: 250, // Default is 300
+          closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+          draggable: true, // Choose whether you can drag to open on touch screens,
+        }
+      );      
 });
